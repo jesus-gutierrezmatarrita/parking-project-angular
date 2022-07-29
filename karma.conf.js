c0bcf08847a2a9cc154ec067223f3adf80d7a30d
Jesús Gutiérrez <jesus.gutierrezmatarrita@outlook.com>
2022-07-28 23:23:58 -0600

    change the order of some files while building

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Florida-Frontend'),
+      dir: require('path').join(__dirname, './coverage/parking-project-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/parking-project-angular'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/parking-project-angular/index.html'));});
+app.get('/*', function (req, res) {
+    console.log(__dirname + '/dist/parking-project-angular/index.html');
+    res.sendFile(path.join(__dirname +
+        '/dist/parking-project-angular/index.html'));
+});
 app.listen(process.env.PORT || 8080);
